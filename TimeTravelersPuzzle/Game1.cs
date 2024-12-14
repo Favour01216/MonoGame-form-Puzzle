bfa6c933d9b7786d80f553591249bd6d237b64ae
neverwasAM <56238781+neverwasAM@users.noreply.github.com>
2024-12-14 06:24:38 -0500

    timer and enum
    made the timer and the enum

--- TimeTravelersPuzzle/Game1.cs
+++ TimeTravelersPuzzle/Game1.cs
@@ -6,8 +6,19 @@ namespace TimeTravelersPuzzle
 {
     public class Game1 : Game
     {
+        // creates the e num for game states 
+        public enum GameState
+        {
+            MainMenu,
+            Playing,
+            GameOver
+        }
+
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Timer _timer;
+        private GameState _currentState;
 
         public Game1()
         {
@@ -27,6 +38,9 @@ namespace TimeTravelersPuzzle
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            _timer = new Timer(60f, _font, new Vector2(10, 10)); //add the font  to this line  where _font is 
+
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -35,6 +49,38 @@ namespace TimeTravelersPuzzle
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
+            _timer.Update(gameTime);
+          
+            //swickh statment to change  betwen menu game and game over
+            switch (_currentState)
+            {
+                case GameState.MainMenu:
+                    
+                    if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                    {
+                        _currentState = GameState.Playing;
+                    }
+                    break;
+
+                case GameState.Playing:
+                    
+                    _timer.Update(gameTime);
+
+                    if (_timer.IsGameOver())
+                    {
+                        _currentState = GameState.GameOver;
+                    }
+                    break;
+
+                case GameState.GameOver:
+                   
+                    if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                    {
+                        _currentState = GameState.MainMenu;
+                    }
+                    break;
+            }
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -43,9 +89,25 @@ namespace TimeTravelersPuzzle
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+           
+            switch (_currentState)
+            {
+                case GameState.MainMenu:
+                    // draw maine menu here 
 
+                    break;
+
+                case GameState.Playing:
+                    //draw game here 
+                    _timer.Draw(_spriteBatch);
+                    break;
+
+                case GameState.GameOver:
+                    // drow game over here
+                    break;
+            }
             // TODO: Add your drawing code here
-
+            _timer.Draw(_spriteBatch);
             base.Draw(gameTime);
         }
     }
